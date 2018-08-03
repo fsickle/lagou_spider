70eed9a86dc06ce02131d2e1ef6a05ca4cf72cfb
fsickle <wanghyscu@163.com>
2018-08-03 14:12:48 +0800

    login

---
+++ analog_landing.py
@@ -0,0 +1,160 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+import json
+from hashlib import md5
+import time
+from urllib.parse import urlencode
+
+class Login():
+    def __init__(self):
+        self.session = requests.session()
+        self.headers = {
+            'Accept-Encoding': 'gzip,deflate,br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Host': 'passport.lagou.com',
+            'Referer': 'https://www.lagou.com/',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0(X11;Linuxx86_64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/68.0.3440.75Safari/537.36'
+        }
+
+    def get_token_code(self):
+        url = 'https://passport.lagou.com/login/login.html'
+        # query_string = {
+        #     'ts': '1533271595716',
+        #     'signature': 'F9415BF569085BC9A80B86B4724959C7',
+        # }
+        # url = url + urlencode(query_string)
+        data = self.session.get(url, headers=self.headers)
+        soup = bs(data.text, 'lxml')
+        '''
+        </script>
+
+            <!-- 页面样式 -->    <!-- 动态token，防御伪造请求，重复提交 -->
+            <script>
+            window.X_Anti_Forge_Token = '13e316e1-8910-4da9-85c2-7e31fe1225b0';
+            window.X_Anti_Forge_Code = '44622791';
+        </script>
+        '''
+
+        anti = soup.find_all('script')[1].string
+        anti_token = {'X_Anti_Forge_Token': 'None',
+                      'X_Anti_Forge_Code': '0'}
+        anti_token['X_Anti_Forge_Token'] = re.findall(r'= \'(.+?)\'', anti)[0]
+        anti_token['X_Anti_Forge_Code'] = re.findall(r'= \'(.+?)\'', anti)[1]
+        return anti_token
+
+    def encryptPwd(self, password):
+        # 对密码的双重加密
+        password = md5(password.encode('utf-8')).hexdigest()
+        password = 'veenike' + password + 'veenike'
+        password = md5(password.encode('utf-8')).hexdigest()
+        return password
+
+    def get_Captcha(self):
+        pass
+
+    def login(self, user, passwd, captchaData=None, token_code=None):
+        url = 'https://passport.lagou.com/login/login.json'
+        passwd = self.encryptPwd(passwd)
+        form_data = {
+            'isValidate': 'true',
+            'username': user,
+            'password': passwd,
+            'request_form_verifyCode': (captchaData if captchaData!=None else ''),
+            'submit': '',
+        }
+        print(form_data)
+        token_code = self.get_token_code() if token_code is None else token_code
+        header = {
+            'Accept-Encoding': 'gzip,deflate,br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Origin': 'https://passport.lagou.com',
+            'Host': 'passport.lagou.com',
+            'Referer': 'https://passport.lagou.com/login/login.html',
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'Mozilla/5.0(X11;Linuxx86_64)AppleWebKit/537.36(KHTML,likeGecko)Chrome/68.0.3440.75Safari/537.36'
+        }
+
+        header.update(token_code)
+        #print(header)
+        #print('ok')
+        response = self.session.post(url, headers=header, data=form_data)
+        data = json.loads(response.content.decode('utf-8'))
+        print(data)
+
+        if data['state'] == 1:
+            return response.content
+        elif data['state'] == 10010:
+            print(data['message'])
+            #captchaData = self.getCaptcha()
+            token_code = {'X_Anti_Forge_Token': data['submitToken'],
+                      'X_Anti_Forge_Code': data['submitCode']}
+            return self.login(username, password, captchaData, token_code)
+        else:
+            print(data['message'])
+            return False
+
+    def get_cookie(self):
+        return requests.utils.dict_from_cookiejar(self.session.cookies)
+
+    def get_ticket(self):
+        #两个重定向
+        url = 'https://passport.lagou.com/grantServiceTicket/grant.html'
+        headers = {
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Host': 'passport.lagou.com',
+            'Referer': 'https://passport.lagou.com/login/login.html',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36'
+        }
+        response = self.session.get(url, headers=headers)
+
+        redir_url = response.next.url
+        print(redir_url)
+        url = re.sub('http:', 'https:', redir_url)
+
+        headers = {
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'zh-CN,zh;q=0.9',
+            'Connection': 'keep-alive',
+            'Host': 'www.lagou.com',
+            'Referer': 'https://passport.lagou.com/login/login.html',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36'
+        }
+        response = self.session.get(url, headers=headers)
+        print(response.status_code)
+        cookies = self.get_cookie()
+        print(cookies)
+        with open('cookie_requests.txt', 'w') as f:
+            f.write(str(cookies))
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    s = Login()
+    a = s.get_token_code()
+    username = '18328592041'
+    password = 'w001~why'
+    s.login(username, password, token_code=a)
+    # s = s.get_cookie()
+    # print(s)
+    # with open('cookie_requests.txt', 'w') as f:
+    #     f.write(str(s))
+
+
+
+
--- lagou_spider.py
+++ lagou_spider.py
@@ -1,8 +1,9 @@
 import requests
 from urllib.parse import urlencode
 import json
+from pyquery import PyQuery as pq
 
-class Download_id():
+class Spider():
     query_string = {
         'px': 'default',
         'city': '成都',
@@ -13,26 +14,56 @@ class Download_id():
         'kd': 'python'
     }
     cookies = {
-        'JSESSIONID': 'ABAAABAAAGGABCB93C3C3FF7A758D774AA3A561151DE137',
-        'SEARCH_ID': 'dad3b6a16b774c2e8d6e86bcdd7b1da9',
         'user_trace_token': '20180802150401-8c175326-f489-470c-a0f1-af016d2c97dd',
+        'SEARCH_ID': 'dad3b6a16b774c2e8d6e86bcdd7b1da9',
+        'JSESSIONID': 'ABAAABAAAGGABCB93C3C3FF7A758D774AA3A561151DE137',
     }
     url_json = 'http://www.lagou.com/jobs/positionAjax.json?'
     url_json = url_json + urlencode(query_string)
     url_referer = 'https://www.lagou.com/jobs/list_python?'
     url_referer = url_referer + urlencode(query_string)
 
-    def download(self):
-        session = requests.session()
-        session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36'
-        session.headers['Referer'] = self.url_referer
-        response = session.post(self.url_json, cookies=self.cookies, data=self.form_data)
+    session = requests.session()
+    session.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Safari/537.36'
+    session.headers['Referer'] = url_referer
 
+    def download_id(self):
+        response = session.post(self.url_json, cookies=self.cookies, data=self.form_data)
         content = json.loads(response.text)
         for i in range(15):
             id = content['content']['positionResult']['result'][i]['positionId']
-            print(id)
+            yield id
+
+    def download_message(self, id):
+        url = 'https://www.lagou.com/jobs/'+str(id)+'.html'
+        print(url)
+        response = self.session.get(url)
+        return response.text
+
+    def parse(self, html):
+        print(html)
+        doc = pq(html)
+        lis = doc('.position-content .job_request span').items()
+        print(lis)
+        list = []
+        for li in lis:
+            list.append(li.text())
+        message = {
+            'company': doc('.position-content .job_name .company').text(),
+            'job_name': doc('.position-content .job-name .name').text(),
+            'job_request': list,
+            'advantage': doc('.job_detail .job_advantage p').text(),
+
+        }
+        print(message)
+
+
+
+
+
 
 if __name__ == '__main__':
-    s = Download_id()
-    s.download()
+    s = Spider()
+    id = s.download_id()
+    response = s.download_message(id)
+    s.parse(response)
